ba6a522913c9d9a65dcc5bd2676e6abfdf9f9cd9
RAKHE YAKUB BAHTIAR <the_best_soneta@yahoo.com>
2016-03-30 10:33:58 +0700

    Added files via upload

---
+++ DeretFibonacci.java
@@ -0,0 +1,19 @@
+package deret.fibonacci;
+
+import java.util.Scanner;
+
+public class DeretFibonacci {
+
+    public static void main(String[] args) {
+    int a=0,b=1,hasil=0,c;
+    Scanner input = new Scanner (System.in);
+    System.out.print ("Masukan Deret Fibonacci  : ");
+        c = input.nextInt ();
+        for(int i=1; i<=c; i++){            
+    a=b;
+    b=hasil;
+    hasil=a+b;
+    System.out.print(hasil+" ");
+        }
+    }
+}
